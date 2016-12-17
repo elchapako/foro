dc5ecf48a0da0a21093bc2322d692e284982d84c
elchapako <el.chapako@gmail.com>
2016-12-17 04:48:55 +0000

    show datails of a register

--- routes/public.php
+++ routes/public.php
@@ -18,3 +18,9 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index');
+
+Route::get('post/{post}', [
+    'as' => 'posts.show',
+    'uses' => 'PostController@show'
+])->where('post', '\d+');
+
