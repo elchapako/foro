4948daaef28b186d298c7dcac2f282e0a0be7d00
elchapako <el.chapako@gmail.com>
2016-12-19 05:40:54 +0000

    regration test

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,12 +3,15 @@
 namespace App\Http\Controllers;
 
 use App\Post;
-use Illuminate\Http\Request;
+
 
 class PostController extends Controller
 {
-    public function show(Post $post)
+    public function show(Post $post, $slug)
     {
+        if($post->slug != $slug){
+            return redirect($post->url, 301);
+        }
         return view('posts.show', compact('post'));
     }
 }
--- routes/public.php
+++ routes/public.php
@@ -19,7 +19,7 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index');
 
-Route::get('post/{post}', [
+Route::get('post/{post}-{slug}', [
     'as' => 'posts.show',
     'uses' => 'PostController@show'
 ])->where('post', '\d+');
