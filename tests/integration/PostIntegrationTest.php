df1e28225410784f378c40ac58f91d30cba330dc
elchapako <el.chapako@gmail.com>
2016-12-20 18:00:23 +0000

    Use model factories to create registers

--- tests/integration/PostIntegrationTest.php
+++ tests/integration/PostIntegrationTest.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Post;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
 
 class PostIntegrationTest extends TestCase
@@ -8,14 +7,9 @@ class PostIntegrationTest extends TestCase
     use DatabaseTransactions;
     public function test_a_slug_is_generated_and_saved_to_the_database()
     {
-        $user = $this->defaultUser();
-
-        $post = factory(Post::class)->make([
+        $post = $this->createPost([
             'title' => 'Como instalar laravel',
         ]);
-
-        $user->posts()->save($post);
-
         $this->assertSame(
             'como-instalar-laravel',
             $post->fresh()->slug
